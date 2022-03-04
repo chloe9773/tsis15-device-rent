fdcac51dc946af672974b2f9e34ca21b5c067baf
pepze21 <pepze21@gmail.com>
2022-03-04 21:43:38 +0900

    220304_001 - [kyeongpyo] login templete and function (store)

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,66 @@
 import { createStore } from 'vuex'
+import router from '../router'
+import axios from 'axios'
 
 export default createStore({
+  // 프로젝트 내 모든 곳에서 사용이 가능한 변수들을 정의
   state: {
+    userInfo: null,
+    isLogin: false,
+    isLoginError: false
   },
   getters: {
   },
+  // commit() 통해 이곳의 함수를 호출
   mutations: {
+    loginSuccess (state, userInfo) {
+      state.isLogin = true
+      state.isLoginError = false
+      state.userInfo = userInfo
+      // console.log('JSON.stringify(userInfo) : ' + JSON.stringify(userInfo))
+      // console.log('JSON.stringify(userInfo) : ' + JSON.stringify(userInfo.LEVEL))
+      alert('login Successed')
+
+      // 관리자인지 여부가 어떤 변수명으로 넘어오는지 몰라서 일단 LEVEL으로 테스트
+      if (JSON.stringify(userInfo.LEVEL) === '"수습사원"') {
+        router.push({ name: 'device-list-board', params: { ID: JSON.stringify(userInfo.ID) } })
+      } else if (JSON.stringify(userInfo.LEVEL) === '"관리자"') {
+        router.push({ name: 'approval-list-board', params: { ID: JSON.stringify(userInfo.ID) } })
+      }
+    },
+    loginError (state) {
+      state.isLogin = false
+      state.isLoginError = true
+    },
+    logout (state) {
+      state.isLogin = false
+      state.isLoginError = false
+      state.userInfo = null
+    }
   },
   actions: {
+    login ({ commit }, loginObj) {
+      // IT사업본부 CPCODE == "G002"
+      loginObj.CPCODE = 'G002'
+      // loginObj ~ {"CPCODE":"G002", "ID":"00000000", "PWD":"password"}
+
+      // 태광 서버 : https://www.t-ammo.com/IGWS/loginLegacy_.json
+      // 15기 서버 : http://133.186.212.200:8080/loginOk.do
+      axios
+        .post('http://133.186.212.200:8080/loginOk.do', loginObj)
+        .then((res) => {
+          // console.log("res JSON : " + JSON.stringify(res))
+          commit('loginSuccess', res.data)
+        })
+        .catch(() => {
+          alert('login failed')
+        })
+    }
   },
-  modules: {
+  logout ({ commit }) {
+    commit('logout')
+    console.log('logout')
+    alert('logout')
+    router.push({ name: 'login' })
   }
 })
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,66 @@
 <template>
-  <div>dd</div>
+  <div class="logo-img-wrap mb-30">
+    <img src="@/assets/img/img_symbol.png" alt="" class="logo" />
+  </div>
+  <div class="login-wrap w-100">
+    <form id="" class="position-center w-30 b-1 p-50 border-radius-3" @submit.prevent="login({ ID, PWD })">
+      <div class="m-auto id-wrap mb-10 w-50 border-radius-2 b-1">
+        <input
+          id="id"
+          name="id"
+          type="text"
+          class="p-10"
+          placeholder="통합그룹웨어 사번"
+          tabindex="1"
+          autofocus
+          v-model="ID"
+        />
+      </div>
+      <div class="m-auto pwd-wrap mb-20 w-50 border-radius-2 b-1">
+        <input
+          id="pwd"
+          name="pwd"
+          type="password"
+          class="p-10"
+          placeholder="비밀번호"
+          tabindex="2"
+          v-model="PWD"
+        />
+      </div>
+      <div class="t-center">
+        <button id="login-btn" name="login-btn" class="submit-btn w-50" @click='loginToDeviceListBoard'>로그인</button>
+      </div>
+    </form>
+    <button id="logout-btn" name="logout-btn" class="w-50" @click="logout()">로그아웃</button>
+  </div>
 </template>
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      id: null,
+      pwd: null
+    }
+  },
+  computed: {
+    ...mapState(['isLogin', 'isLoginError'])
+  },
+  methods: {
+    ...mapActions(['login', 'logout'])
+  }
+}
+</script>
+
+<style lang="css">
+@import "@/assets/css/reset.css";
+@import "@/assets/css/common.css";
+@import "@/assets/css/head.css";
+.login-wrap>form {
+  box-shadow: 3px 2px 2px #c5c5c5;
+}
+input {
+  border: 1px solid transparent;
+}
+</style>
