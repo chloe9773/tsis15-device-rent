14e44a57a6c71c179b2da46fe91998cc3ef5f3d1
hmj <domestic94@gmail.com>
2022-03-08 22:12:03 +0900

    20220308_002 - [hmj] 디바이스 리스트화면 작업 (모달작업 일부)

--- src/components/DeviceDetail.vue
+++ src/components/DeviceDetail.vue
@@ -1,8 +1,8 @@
 <template>
-<DeviceDetail v-bind:selected="selected" v-if ="renderComp === true"/>
-<!-- <Modal>
+<Modal>
     <template #modal-content-top class="form">
         <div class="b-1">
+            <!-- <div v-for="s in selected" v-bind:key="s">{{s}}</div> -->
         <div class="form-head d-flex space-between mb-2">
             <div class="form-title f-24 font-700 p-20 t-center w-65" style="padding-top:40px;">단말기 임대 품의서</div>
             <div class="form-approval-wrap d-flex">
@@ -46,7 +46,7 @@
             </div>
         </div>
         <div class="form-body">
-            <table class="form f-14 w-100" id="approvalTable">
+            <table class="form f-14 w-100">
             <tr>
                 <td class="table-title">일련번호</td>
                 <td>티시스-IT사업본부-A072</td>
@@ -54,11 +54,11 @@
                 <td>2022/03/01</td>
             </tr>
             <tr class="space"></tr>
-            <tr id="formbodytr">
+            <tr>
                 <td class="table-title">단말기</td>
-                <td colspan="3" name ="device" id ="deviceID">
-                  {{this.list[0].item_id}}
-                </td>
+                <td colspan="3" name ="device" id ="deviceID" > {{this.selected[0]["name"]}}</td>
+                <!-- <td colspan="3" name ="device" id ="deviceID"> {{this.selected[0].length}}</td> -->
+
             </tr>
             <tr>
                 <td rowspan="3" class="table-title">임대정보</td>
@@ -110,136 +110,27 @@
             <button id="extensionBtn" class="active-btn" v-on:click="toggleModal('approval',1)">연장하기</button>
         </div>
     </template>
-</Modal> -->
-    <div class="main-content-admin w-70 m-auto">
-        <div class="table-title- mb-20 d-flex space-between">
-            <div class="table-title-left d-flex w-70">
-                <select name="" id="" class="p-5 mr-5">
-                    <option value="search-keyword">단말기명</option>
-                </select>
-                <div class="input-wrap p-5 b-1 w-70">
-                    <input type="text" class="" />
-                    <span class="float-r mr-5">search</span>
-                </div>
-            </div>
-            <div class="table-title-right" style="transform:translateY(20%);">
-                <span class="f-14" >대여 가능 항목만 보기</span>
-                <input type="checkbox" name="" id="usecheckbox" class="" @click="availableChecked">
-            </div>
-        </div>
-        <div class="table-wrap w-100">
-            <table class="table w-100" id="device-list-table">
-                <thead class="f-14 font-700 bb-2 bt-2">
-                    <th class="p-20">종류</th>
-                    <th class="v-middle">브랜드</th>
-                    <th class="v-middle">단말기명</th>
-                    <th class="v-middle">대여 현황</th>
-                    <th class="v-middle">
-                        <button class="active-btn" v-on:click="toggleModal('approval',2)">대여하기</button>
-                    </th>
-                    <th class="v-middle hiddencol">
-                      <input type="hidden">
-                    </th>
-                </thead>
-                <tbody class="f-13 t-center" id="ttt">
-                  <tr :key="index" v-for="(item,index) in list" class="bb-1 cursor tabletr" >
-                    <td class="w-15" id="category" >{{item.category}}</td>
-                    <td class="w-10" id="brand" >{{item.brand}}</td>
-                    <td id="name" >{{item.name}}</td>
-                    <td class="w-15 grn" id="status"> {{item.status}}</td>
-                    <td class="w-15">
-                      <input type="checkbox" class="devicecheck" v-model= "selected" :value= item @change="tmpFunc()"/>
-                    </td>
-                    <td class="w-10 hiddencol">
-                      <input type="hidden" :value = item.serialnum />
-                    </td>
-                  </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+</Modal>
 </template>
 
-
 <script>
-import axios from 'axios'
 import Modal from '@/components/Modal.vue'
-import DeviceDetail from '@/components/DeviceDetail.vue'
 
 export default {
-  // 창이 열리자마자 데이터 받아와서 담기
-  created: function(){
-    axios.get('http://133.186.212.200:8080/item/paging/1').then(res => {
-      this.list=res.data.list
-  }).catch(res => {
-    console.log(res)
-  })
-  },
-  
-  name: 'DeviceList',
+  name: 'DeviceDetail',
   components: {
-    Modal,
-    DeviceDetail
-  },
-
-  // 각 row 체크 : v-model = "selected"
-  data () {
-    return {
-      list: [],
-      selected: [],
-      slides: document.getElementsByClassName('tabletr'),
-      item_id: [],
-      serialnum: [],
-      name: [],
-      brand: [],
-      category: [],
-      status: [],
-      pagnum: 1,
-      slist: [],
-      test: [],
-      renderComp: false
-    }
-  },
+    Modal
+  }, 
   methods: {
-    tmpFunc() {
-      this.renderComp = true
-    },
     toggleModal (category, type) {
-      // this.renderComp = true;
-      console.log("modal: " + this.selected[0].item_id)
-      this.test = this.selected
-      console.log("modal test: " + this.test[0].item_id)
-      // console.log(document.getElementById("approvalTable").getElementsByTagName("tr").length)
-      console.log(this.renderComp)
       this.$toggleModal(category, type)
     },
-    // 대여 가능 항목만 보기 체크
-    availableChecked: function () {
-      alert('대여가능항목눌림' + "T&F : " + document.getElementById('usecheckbox').checked + "///////"+this.slides)
-      if (document.getElementById('usecheckbox').checked === true) {
-        for (let i=0; i<this.slides.length;i++) {
-          if(this.slides[i].getElementsByTagName("td")[3].innerText==='예약중') {
-            this.slides[i].style.display='none'
-          }
-        }
-      } else {
-          for (let i=0; i<this.slides.length;i++){
-            if(this.slides[i].getElementsByTagName("td")[3].innerText==='예약중'){
-            this.slides[i].style.display=''
-          }
-        }
-      }
-    }
-    // toggleModal (category, type) {
-    //   this.$toggleModal(category, type)
-    // }
-  }
-  
+  },
+  props: {
+      selected : Array
+  },
+  created() {
+      console.log("modalCreated : "+this.list)
+  },
 }
 </script>
-
-<style scoped>
-.hiddencol {
-  display: none;
-}
-</style>
--- src/views/CommonView.vue
+++ src/views/CommonView.vue
@@ -1,16 +1,21 @@
 <template>
 <MenuBar/>
 <DeviceList/>
+<PageBtn/>
+<DeviceDetail/>
 </template>
 <script>
 import MenuBar from '@/components/MenuBar.vue'
 import DeviceList from '@/components/DeviceList.vue'
-
+import PageBtn from '@/components/table/PageBtn.vue'
+import DeviceDetail from '@/components/DeviceDetail.vue'
 export default {
   name: 'CommonView',
   components: {
     MenuBar,
-    DeviceList
+    DeviceList,
+    PageBtn,
+    DeviceDetail
   }
 }
 </script>
