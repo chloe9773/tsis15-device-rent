ebf2ce2265eb7d5ee0eed4ba1e26f2e9cfded81f
pepze21 <pepze21@gmail.com>
2022-03-08 19:47:46 +0900

    20220308_001 - [kyeongpyo] 공지사항게시판 기능구현(페이징 컴포넌트 구현 포함)

--- src/components/table/PageBtn.vue
+++ src/components/table/PageBtn.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="page-wrap f-12 t-center mt-20">
-    <span class="pg-btn p-5 disable">처음으로</span>
-    <span class="pg-btn disable">이전</span>
-    <span class="pg-btn active">1</span>
-    <span class="pg-btn">2</span>
-    <span class="pg-btn">3</span>
-    <span class="pg-btn">4</span>
-    <span class="pg-btn">5</span>
-    <span class="pg-btn">6</span>
-    <span class="pg-btn">7</span>
-    <span class="pg-btn">8</span>
-    <span class="pg-btn">9</span>
-    <span class="pg-btn">10</span>
-    <span class="pg-btn">다음</span>
-    <span class="pg-btn">끝으로</span>
+    <span v-if="currPage == 1" class="pg-btn p-5 disable">
+      처음으로
+    </span>
+    <span v-else class="pg-btn p-5" @click="$emit('getDataByBtn', 1, keyword)">
+      처음으로
+    </span>
+    <span v-if="currPage == 1" class="pg-btn disable">
+      이전
+    </span>
+    <span v-else class="pg-btn" @click="$emit('getDataByBtn', currPage - 1, keyword)">
+      이전
+    </span>
+    <span class="pg-num-wrap"
+      :key="index"
+      v-for="(page_num,index) in pageList"
+      @click="$emit('getDataByBtn', page_num, keyword)">
+      <span v-if="page_num == currPage" class="pg-btn active">
+        {{page_num}}
+      </span>
+      <span v-else class="pg-btn">
+        {{page_num}}
+      </span>
+    </span>
+    <span v-if="currPage == pages" class="pg-btn disable">
+      다음
+    </span>
+    <span v-else class="pg-btn" @click="$emit('getDataByBtn', currPage + 1, keyword)">
+      다음
+    </span>
+    <span v-if="currPage == pages" class="pg-btn disable">
+      끝으로
+    </span>
+    <span v-else class="pg-btn" @click="$emit('getDataByBtn', pages, keyword)">
+      끝으로
+    </span>
   </div>
-</template> 
+</template>
+<script>
+export default {
+  props: {
+    pageList: Array,
+    currPage: Number,
+    keyword: String,
+    pages: Number
+  },
+  emits: ['getListBySearchBtn']
+}
+</script>
