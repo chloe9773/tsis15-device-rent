b46c70b3460cf00611d88af3ed2c73767030129a
chloe9773 <jeonghwapark@outlook.com>
2022-03-10 14:20:01 +0900

    20220310_001 - [jeonghwa] modify login

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -16,9 +16,13 @@
           <router-link to="/notice-board/list">공지사항</router-link>
         </div>
       </div>
-      <div class="nav-right">
-        <div class="p-20 menu">
+      <div class="nav-right d-flex space-between bg-highlight white f-18 font-700">
+        <div v-bind:class="[{'p-20 menu d-none': role <= 1}, {'p-20 menu cursor': role >= 2}]"> 
+          <!-- this.$cookies.get("user_role") -->
           <router-link to="/admin-board/admin">관리자</router-link>
+        </div>
+        <div class="p-20 menu cursor" v-on:click.prevent="logout">
+          <span>로그아웃</span>
         </div>
       </div>
     </div>
@@ -31,3 +35,27 @@
   @import url("@/assets/css/head.css");
   @import url("@/assets/css/admin.css");
 </style>
+
+<script>
+  export default {
+  created() {
+    if(this.$cookies.get('user_role') != undefined || this.$cookies.get('user_role') != null || this.$cookies.get('user_role') != "") this.role = this.$cookies.get('user_role')
+  },
+  data() {
+    return {
+      role: 0
+    }
+  },
+  methods: {
+    logout() {
+      // this.$router.push({name: "login"}) // 왜 로그인으로는 안가는지?
+      this.$cookies.remove("user_id")
+      this.$cookies.remove("user_name")
+      this.$cookies.remove("user_role")      
+
+      alert("로그아웃 되었습니다.")
+      this.$router.push({ path : "/"})
+    }
+  }
+}
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,9 @@
-<template>
+  <template>
   <div class="logo-img-wrap mb-30">
     <img src="@/assets/img/img_symbol.png" alt="" class="logo" />
   </div>
   <div class="login-wrap w-100">
-    <form id="" class="position-center w-30 b-1 p-50 border-radius-3" @submit.prevent="login({ ID, PWD })">
+    <form id="" class="position-center w-30 b-1 p-50 border-radius-3" @submit.prevent="login()">
       <div class="m-auto id-wrap mb-10 w-50 border-radius-2 b-1">
         <input
           id="id"
@@ -31,25 +31,71 @@
         <button id="login-btn" name="login-btn" class="submit-btn w-50">로그인</button>
       </div>
     </form>
-    <button id="logout-btn" name="logout-btn" class="w-50" @click="logout()">로그아웃</button>
   </div>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex'
+import axios from 'axios'
+// import { mapState, mapActions } from 'vuex'
 
 export default {
-  data () {
-  },
-  computed: {
-    ...mapState(['isLogin', 'isLoginError'])
+  data() {
+    return {
+      ID: "",
+      PWD: "",
+      CPCODE: "G002"
+    }
   },
   methods: {
-    ...mapActions(['login', 'logout'])
+    login() {
+      let data = {
+        ID: this.ID,
+        PWD: this.PWD,
+        CPCODE: this.CPCODE
+      }
+
+      let router = this.$router;
+
+      axios
+        .post("http://133.186.212.200:8080/loginOk.do", data)
+        .then((res) => {
+          if(res.status == 200) {
+            if (res.data.RESULT == 'FAIL') {
+              alert("올바른 정보를 입력해주세요.")
+            } else {
+              this.$cookies.set("user_id", res.data.ID)
+              this.$cookies.set("user_name", res.data.NAME)
+              this.$cookies.set("user_dept", res.data.CP_NAME)
+              this.$cookies.set("user_role", res.data.ROLE)
+              
+              if (this.$cookies.get("user_role") > 1) router.push({ path : "/approval-list-board"})
+              else router.push({ path : "/device-list-board"})
+            }
+          }
+        })
+        .catch((err) => {
+          //alert("예기치 못한 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.")
+        })
+    }
   }
+  // data () {
+  //   return {
+  //     id: null,
+  //     pwd: null
+  //   }
+  // },
+  // computed: {
+  //   ...mapState(['isLogin', 'isLoginError'])
+  // },
+  // methods: {
+  //   ...mapActions(['login', 'logout'])
+  // }
 }
 </script>
 
 <style lang="css">
+@import "@/assets/css/reset.css";
+@import "@/assets/css/common.css";
+@import "@/assets/css/head.css";
 .login-wrap>form {
   box-shadow: 3px 2px 2px #c5c5c5;
 }
